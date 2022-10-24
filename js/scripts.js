2ac419a50c7dcfa7d7b2d6099b32d3437c3cd20b
Sanket Mistry <mistry.sanket@gmail.com>
2022-10-24 11:51:56 +0400

    TEst

--- js/scripts.js
+++ js/scripts.js
@@ -200,7 +200,7 @@ $(document).ready(function () {
             address: 'LOTUS 1 & 2, RAGHULEELA BANQUETS, MUMBAI, INDIA',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Rajesh Mistry at +91 9820309032."
         }
     });
 
